a66cfe4210c78aa8a7625d2e01ad837e804edbfb
Raphaël Claasen <raphael.claasen@gmail.com>
2021-04-21 17:01:44 +0200

    Spec measurements having an associated meter

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:measurement) { build(:measurement) }
+
+  subject { measurement }
+
+  describe "when it has a parent meter" do
+    before { measurement.meter = create(:meter) }
+
+    it { is_expected.to be_valid }
+  end
+
+  describe "when it does not have a parent meter" do
+    before { measurement.meter = nil }
+
+    it { is_expected.to_not be_valid }
+  end
 end
