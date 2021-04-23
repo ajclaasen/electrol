4e904b57cd97f821a4a8243cd035060b093d6301
Raphaël Claasen <raphael.claasen@gmail.com>
2021-04-23 12:29:44 +0200

    Fix show haml spec

--- spec/views/meters/show.html.haml_spec.rb
+++ spec/views/meters/show.html.haml_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "meters/show", type: :view do
   before(:each) do
-    @meter = assign(:meter, create(:meter))
+    @meter = assign(:meter, create(:meter, name: "MName", interval: 8.days, unit: "MUnit"))
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Interval/)
-    expect(rendered).to match(/Unit/)
+    expect(rendered).to match(/MName/)
+    expect(rendered).to match(/8 days/)
+    expect(rendered).to match(/MUnit/)
   end
 
   describe "when it has a measurement" do
