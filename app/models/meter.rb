87f9d03e8fc4ea4b6dcf501d998f3b6eddbbbfd8
Raphaël Claasen <raphael.claasen@gmail.com>
2021-04-26 16:03:03 +0200

    Reject measurements without id attribute

--- app/models/meter.rb
+++ app/models/meter.rb
@@ -1,6 +1,6 @@
 class Meter < ApplicationRecord
   has_many :measurements, dependent: :destroy
-  accepts_nested_attributes_for :measurements
+  accepts_nested_attributes_for :measurements, reject_if: proc { |atts| atts['id'].blank? }
 
   # Ensures interval returns an `ActiveSupport::Duration` object.
   # Default behavior (thus, an obsolete line of code) from Rails 6.2 onwards.
